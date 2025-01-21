502438dfcd18c3641d1f8488a91d833cc809ab97
Mikhail Volzhanin <mishavolzhanin.mishavolzhanin2@gmail.com>
2025-01-22 00:14:09 +0300

    feat: create user entity with repository

---
+++ src/main/java/ru/volzhanin/deliverybackendapplication/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package ru.volzhanin.deliverybackendapplication.repository;
+
+import ru.volzhanin.deliverybackendapplication.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationCode(String verificationCode);
+}
