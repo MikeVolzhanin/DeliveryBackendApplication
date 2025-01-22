992cdef681f4e8be4ff801a67b1420aa60c98969
Mikhail Volzhanin <mishavolzhanin.mishavolzhanin2@gmail.com>
2025-01-22 12:52:24 +0300

    feat: create user service

---
+++ src/main/java/ru/volzhanin/deliverybackendapplication/service/UserService.java
@@ -0,0 +1,23 @@
+package ru.volzhanin.deliverybackendapplication.service;
+
+import ru.volzhanin.deliverybackendapplication.entity.User;
+import org.springframework.stereotype.Service;
+import ru.volzhanin.deliverybackendapplication.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers(){
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+}
