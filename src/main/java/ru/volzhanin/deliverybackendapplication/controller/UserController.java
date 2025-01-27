02b2129ba2275e460a5154b5b37719808805b204
Mikhail Volzhanin <mishavolzhanin.mishavolzhanin2@gmail.com>
2025-01-27 14:16:47 +0300

    refactor: remove one user controller

--- src/main/java/ru/volzhanin/deliverybackendapplication/controller/UserController.java
+++ src/main/java/ru/volzhanin/deliverybackendapplication/controller/UserController.java
@@ -1,6 +1,5 @@
 package ru.volzhanin.deliverybackendapplication.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,27 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.volzhanin.deliverybackendapplication.service.UserService;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping("/me")
-    public ResponseEntity<User> authenticatedUser() {
+    public ResponseEntity<String> authenticatedUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User currentUser = (User) authentication.getPrincipal();
-        return ResponseEntity.ok(currentUser);
-    }
-
-    @GetMapping("/")
-    public ResponseEntity<?> allUsers() {
-        List<User> users = userService.allUsers();
-        return ResponseEntity.ok(users);
+        return ResponseEntity.ok(currentUser.getFirstName());
     }
 }
